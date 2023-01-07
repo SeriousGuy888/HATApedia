7fd5e3efff4db15e8b1b272c532ecc186b044cc1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-01-07 01:21:16 -0500

    material ui grid of nations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,63 @@
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
-import { AppBar, Toolbar, useTheme } from "@mui/material"
+import {
+  AppBar,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Toolbar,
+  useTheme,
+} from "@mui/material"
+
+const data = [
+  {
+    name: "Billzoplace",
+    type: "City State",
+    id: "billzoplace",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "remy_republic",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "tobytopia",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "aaa",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "bbb",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "ccc",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "ddd",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "eee",
+  },
+  {
+    name: "Remy Republic",
+    type: "Federal Republic",
+    id: "fff",
+  },
+]
 
 export default function Home() {
   const theme = useTheme()
@@ -13,24 +70,55 @@ export default function Home() {
         </Toolbar>
       </AppBar>
       <main>
-        <div>
-          <Container>
-            <Typography
-              variant="h2"
-              color={theme.palette.text.primary}
-              marginTop={theme.spacing(4)}
-            >
-              States
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              color={theme.palette.text.secondary}
-              gutterBottom
-            >
-              Empires on the server
-            </Typography>
-          </Container>
-        </div>
+        <Container>
+          <Typography
+            variant="h2"
+            color={theme.palette.text.primary}
+            marginTop={theme.spacing(4)}
+          >
+            Political Entities
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color={theme.palette.text.secondary}
+            gutterBottom
+          >
+            States and empires on the server
+          </Typography>
+
+          <Grid marginY={4} container spacing={2}>
+            {data.map((nation) => (
+              <Grid item key={nation.id} xs={12} sm={6}>
+                <Card variant="outlined">
+                  <CardActionArea
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                    href={`./${nation.id}`}
+                  >
+                    <CardContent sx={{ flex: "4" }}>
+                      <Typography variant="h5">{nation.name}</Typography>
+                      <Typography
+                        variant="caption"
+                        color={theme.palette.text.secondary}
+                      >
+                        {nation.type}
+                      </Typography>
+                    </CardContent>
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        flex: "1",
+                        borderRadius: 1,
+                        imageRendering: "pixelated",
+                      }}
+                      image="/images/banners/billzoplace.png"
+                      alt="Banner"
+                    />
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
   )
