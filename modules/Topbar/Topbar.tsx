a70836a884c99b0ed970103fa29880b20eaced74
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-01-29 20:04:53 -0500

    favicon and logo

--- modules/Topbar/Topbar.tsx
+++ modules/Topbar/Topbar.tsx
@@ -1,18 +1,25 @@
 import Link from "next/link"
+import Image from "next/image"
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher"
 
 const Topbar = () => {
   return (
     <nav className="flex justify-between items-center sticky bg-blue-900 py-4 px-8">
-      <Link href="/" style={{ textDecoration: "none" }}>
+      <Link href="/" className="flex gap-2 items-center no-underline">
+        <Image
+          src="/images/earth_icon.png"
+          alt="Logo"
+          width={32}
+          height={32}
+          style={{
+            imageRendering: "pixelated",
+          }}
+        ></Image>
         <span className="text-white text-2xl tracking-wide font-bold">
           HATApedia
         </span>
       </Link>
       <ThemeSwitcher />
-      {/* <button className="rounded-full bg-blue-600 text-white px-4 py-2">
-        Toggle Theme
-      </button> */}
     </nav>
   )
 }
