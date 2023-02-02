46ef05b4a9df302091dd83035e9387427c96ef0b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-02-02 13:01:57 -0500

    add bounds to the map

--- modules/WorldMap/WorldMap.tsx
+++ modules/WorldMap/WorldMap.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-leaflet"
 import L from "leaflet"
 import "leaflet/dist/leaflet.css"
+import { useEffect, useState } from "react"
 
 const MapEvents = () => {
   useMapEvents({
@@ -18,7 +19,27 @@ const MapEvents = () => {
 }
 
 const WorldMap = () => {
-  const icon = L.icon({
+  const [map, setMap] = useState<L.Map | null>(null)
+
+  const mapSW = L.point(0, 8192)
+  const mapNE = L.point(8192, 0)
+  const mapBoundsZoomLvl = 5
+
+  useEffect(() => {
+    if (!map) {
+      return
+    }
+
+    const bounds = new L.LatLngBounds(
+      map.unproject(mapSW, mapBoundsZoomLvl),
+      map.unproject(mapNE, mapBoundsZoomLvl),
+    )
+    console.log(map.getMaxZoom())
+
+    map.setMaxBounds(bounds)
+  }, [map])
+
+  const markerIcon = L.icon({
     iconUrl: "/images/map/markers/banner.png",
     iconSize: [16, 16],
     iconAnchor: [8, 16],
@@ -39,15 +60,18 @@ const WorldMap = () => {
         backgroundColor: "black",
         imageRendering: "pixelated",
       }}
+      crs={L.CRS.Simple}
+      ref={setMap}
     >
+      <MapEvents />
       <TileLayer
         attribution="HATA SMP World Map"
         url="/map_tile/{z}/{x}/{y}.png"
         noWrap
+        minNativeZoom={1}
         maxNativeZoom={5}
       />
-      <MapEvents />
-      <Marker position={[0, 0]} icon={icon} draggable>
+      <Marker position={[0, 0]} icon={markerIcon} draggable>
         <Popup>Forgsville</Popup>
       </Marker>
     </MapContainer>
