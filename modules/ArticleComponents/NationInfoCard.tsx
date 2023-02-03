d91fa4fe5e51391f476626269b959dd4053cd850
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-02-03 12:00:17 -0500

    fixed banners in nation info cards

--- modules/ArticleComponents/NationInfoCard.tsx
+++ modules/ArticleComponents/NationInfoCard.tsx
@@ -14,15 +14,18 @@ const NationInfoCard: NextPage<{ nation: NationInfoCardData }> = ({
   return (
     <div className="p-6 mb-16 bg-gray-100 dark:bg-gray-800 rounded-lg">
       <section className="flex flex-wrap justify-between items-stretch gap-4">
-        <div className="flex-1 min-w-[60px]">
-          <NationBanner src={nation.banner ?? ""} />
-        </div>
+        <NationBanner src={nation.banner ?? ""} />
         <div className="flex flex-col items-stretch content-start flex-[4] gap-2 min-w-[150px]">
           {nation.info &&
             Object.keys(nation.info).map((key) => (
-              <div className="flex-1 bg-gray-200 dark:bg-gray-700 rounded-md p-4" key={key}>
+              <div
+                className="flex-1 bg-gray-200 dark:bg-gray-700 rounded-md p-4"
+                key={key}
+              >
                 <div className="px-2 py-1">
-                  <p className="uppercase text-gray-600 dark:text-gray-300 text-xs">{key}</p>
+                  <p className="uppercase text-gray-600 dark:text-gray-300 text-xs">
+                    {key}
+                  </p>
                   <p className="text-md">{nation?.info[key] ?? "Unknown"}</p>
                 </div>
               </div>
