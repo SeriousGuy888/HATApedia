353ac72e48f6a2551137e1fc0a8aef1573b9156c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-02-06 11:16:06 -0500

    display article images on the article page

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import Image from "next/image"
 import { GetStaticPathsResult, GetStaticPropsResult, NextPage } from "next"
 import { Article, getAllSlugs, getArticle } from "../../lib/articlesApi"
 import NationInfoCard from "../../modules/ArticleComponents/NationInfoCard"
@@ -28,13 +29,20 @@ const ArticlePage: NextPage<Props> = ({ article, html }) => {
         <title>{`${article.title} - HATApedia`}</title>
       </Head>
       <div className="max-w-[95vw] md:max-w-prose w-full h-fit p-8">
-        <div className="flex-1 self-start">
-          <h1 className="text-5xl font-bold mb-2">{article.title}</h1>
-          <p className="text-gray-600 dark:text-gray-400 uppercase text-sm">
-            {article.subtitle}
-          </p>
-          <hr className="my-6 border-t-[1] border-gray-200 dark:border-gray-700" />
+        <div className="flex-1 self-start flex justify-between">
+          <div className="">
+            <h1 className="text-5xl font-bold mb-2">{article.title}</h1>
+            <h2 className="text-gray-600 dark:text-gray-400 uppercase text-sm">
+              {article.subtitle}
+            </h2>
+          </div>
+          {article.image && !article.nation && (
+            <div className="rounded-xl p-2 bg-gray-100 dark:bg-gray-800">
+              <Image src={article.image} alt="" width={100} height={100} />
+            </div>
+          )}
         </div>
+        <hr className="my-6 border-t-[1] border-gray-200 dark:border-gray-700" />
 
         {article.nation && <NationInfoCard nation={article.nation} />}
         <article
