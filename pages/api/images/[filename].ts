02ec76e6a82555f47bd1f048b328c8bfbd202f63
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-02 12:09:40 -0400

    use express to serve static files for image endpoint

--- pages/api/images/[filename].ts
+++ pages/api/images/[filename].ts
@@ -1,34 +1,15 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-import fs from "fs"
 import path from "path"
+import express from "express"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Get the image name from the query parameters
-  const fileName = req.query.filename as string
-
-  // Construct the path to the image file
-  const filePath = path.join(process.cwd(), "content", "images", fileName)
-
-  if (!fs.existsSync(filePath)) {
-    res.status(404).send("Image not found")
-    return
-  }
-
-  // Read the image file
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      // Handle error
-      res.status(500).send("Internal server error while reading image")
-    } else {
-      // Set the content type header
-      res.setHeader(
-        "Content-Type",
-        `image/${path.extname(filePath).substring(1)}`,
-      )
-
-      // Send the image data as a response
-      res.send(data)
-    }
-  })
+export const config = {
+  api: { externalResolver: true },
 }
+
+// Construct the path to the image file
+const filePath = path.join(process.cwd(), "content", "images")
+const serveFiles = express.static(filePath)
+
+const handler = express()
+handler.use(["/api/images"], serveFiles)
+
+export default handler
