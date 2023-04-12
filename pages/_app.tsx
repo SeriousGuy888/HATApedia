2d5bccd159592d5fa14f41cbbc1e70fc8c0fd8d1
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-12 19:30:57 -0400

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app"
 import { motion } from "framer-motion"
 import { useRouter } from "next/router"
 import Topbar from "../modules/Topbar/Topbar"
+import { Analytics } from "@vercel/analytics/react"
 
 import "../common/globals.scss"
 import { ThemeProvider } from "next-themes"
@@ -12,25 +13,29 @@ export default function MyApp(props: AppProps) {
   const router = useRouter()
 
   return (
-    <ThemeProvider attribute="class">
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        {/* PWA primary color */}
-        <meta name="theme-color" content="#222999" />
-        <meta property="og:site_name" content="HATApedia" />
-      </Head>
-      <div className="grid grid-rows-[4rem_1fr_1fr] min-h-screen h-screen max-h-screen">
-        <Topbar />
-        <motion.main
-          key={router.route}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          className="flex row-span-2 justify-center"
-        >
-          <Component {...pageProps} />
-        </motion.main>
-      </div>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class">
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          {/* PWA primary color */}
+          <meta name="theme-color" content="#222999" />
+          <meta property="og:site_name" content="HATApedia" />
+        </Head>
+        <div className="grid grid-rows-[4rem_1fr_1fr] min-h-screen h-screen max-h-screen">
+          <Topbar />
+          <motion.main
+            key={router.route}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="flex row-span-2 justify-center"
+          >
+            <Component {...pageProps} />
+          </motion.main>
+        </div>
+      </ThemeProvider>
+
+      <Analytics />
+    </>
   )
 }
