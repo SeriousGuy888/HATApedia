66e74f6f1548512dee13940998c81f257046a748
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-12 11:57:03 -0400

    highlight active heading in toc

--- modules/ArticleComponents/FloatingTableOfContents.tsx
+++ modules/ArticleComponents/FloatingTableOfContents.tsx
@@ -1,6 +1,9 @@
+// https://claritydev.net/blog/nextjs-blog-remark-interactive-table-of-contents
+
 import type { NextPage } from "next"
-import type { ReactNode } from "react"
 import type { TocNode } from "../../plugins/remark-heading-tree"
+import type { ReactNode } from "react"
+import { useEffect, useRef, useState } from "react"
 import Link from "next/link"
 
 const FloatingTableOfContents: NextPage<{ nodes: TocNode[] }> = ({ nodes }) => {
@@ -23,11 +26,37 @@ const FloatingTableOfContents: NextPage<{ nodes: TocNode[] }> = ({ nodes }) => {
   )
 }
 
+function useHighlighted(id: string) {
+  const observer = useRef<IntersectionObserver | null>(null)
+  const [activeId, setActiveId] = useState("")
+
+  useEffect(() => {
+    const handleObserver = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry?.isIntersecting) {
+          setActiveId(entry.target.id)
+        }
+      })
+    }
+
+    observer.current = new IntersectionObserver(handleObserver, {
+      rootMargin: "0% 0% -40% 0px",
+    })
+
+    const elements = document.querySelectorAll("h1,h2,h3,h4,h5,h6")
+
+    elements.forEach((elem) => observer.current?.observe(elem))
+    return () => observer.current?.disconnect()
+  }, [])
+
+  return [activeId === id, setActiveId] as [boolean, (id: string) => void]
+}
+
 function renderNodes(nodes: TocNode[]): ReactNode {
   return (
     <ul>
       {nodes.map((node) => (
-        <li key={node.id} className={`${node.depth > 1 ? "ml-4" : ""}`}>
+        <li key={node.id} className={`${node.depth > 1 ? "" : ""}`}>
           <TocLink node={node} />
           {node.children?.length > 0 && renderNodes(node.children)}
         </li>
@@ -37,16 +66,24 @@ function renderNodes(nodes: TocNode[]): ReactNode {
 }
 
 const TocLink: NextPage<{ node: TocNode }> = ({ node }) => {
+  const [highlighted, setHighlighted] = useHighlighted(node.id)
+
   return (
-    <Link
-      href={`#${node.id}`}
-      className={`${
-        node.depth === 1 ? "text-sm" : "text-xs"
-      } hover:text-blue-600 dark:hover:text-blue-300`}
-      scroll={false}
+    <p
+      className={`text-xs ${
+        highlighted && "bg-gray-200 dark:bg-gray-700"
+      } w-full py-1`}
+      style={{ paddingLeft: `${node.depth - 1}rem` }}
     >
-      {node.value}
-    </Link>
+      <Link
+        href={`#${node.id}`}
+        className={`hover:text-blue-600 dark:hover:text-blue-300`}
+        scroll={false}
+        onClick={() => setHighlighted(node.id)}
+      >
+        {node.value}
+      </Link>
+    </p>
   )
 }
 
