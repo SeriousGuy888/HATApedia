9a9dc39ee0e023ef660b79c293d9364a183cee4e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-14 13:25:01 -0400

    blurring loading images for real for real this time

--- modules/ArticleComponents/MDXComponents.tsx
+++ modules/ArticleComponents/MDXComponents.tsx
@@ -40,40 +40,20 @@ const MDXComponents = {
 
 const BlurredImage = (imgProps: ImageProps) => {
   const [loaded, setLoaded] = useState(false)
-  const realImg = useRef<HTMLImageElement>(null)
-  useEffect(() => {
-    if (loaded) {
-      realImg.current?.classList.remove("h-0")
-    }
-  }, [loaded])
 
   return (
-    <>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src={"/api/images/blur/" + imgProps.src}
-
-        title={imgProps.title}
-        alt={imgProps.alt}
-        width={700}
-        height={350}
-        className={loaded ? "h-0" : ""}
-
-      />
-      <Image
-        src={"/api/images/" + imgProps.src}
-        
-        title={imgProps.title}
-        alt={imgProps.alt}
-        width={700}
-        height={350}
-        
-        className={(imgProps.className ?? "") + loaded ? "" : "h-0"}
-        ref={realImg}
-        onLoadingComplete={() => setLoaded(true)}
-        priority={false}
-      />
-    </>
+    <Image
+      src={"/api/images/" + imgProps.src}
+      blurDataURL={"/api/images/blur/" + imgProps.src}
+      placeholder="blur"
+      title={imgProps.title}
+      alt={imgProps.alt}
+      width={700}
+      height={350}
+      className={imgProps.className ?? ""}
+      onLoadingComplete={() => setLoaded(true)}
+      style={{ filter: loaded ? "" : "blur(10px)" }}
+    />
   )
 }
 
