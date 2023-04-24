fec259bc794a368cfea11a77441bb865ef0b5eb2
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-24 18:20:41 -0400

    load closed captions by default in youtube iframes

--- modules/ArticleComponents/Youtube.tsx
+++ modules/ArticleComponents/Youtube.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from "next"
 
 const Youtube: NextPage<YoutubeEmbedData> = ({ videoId }) => {
   return (
+    // https://developers.google.com/youtube/player_parameters#Parameters
     <iframe
       className="w-full aspect-video"
-      src={`https://www.youtube.com/embed/${videoId}`}
+      src={`
+        https://www.youtube.com/embed/${videoId}
+          ?rel=0
+          &cc_load_policy=1
+          &cc_lang_pref=en
+      `}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
