0d5c1fef55bed1ce38da8c72cf9a23245807fac5
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-24 18:22:58 -0400

    Update Youtube.tsx

--- modules/ArticleComponents/Youtube.tsx
+++ modules/ArticleComponents/Youtube.tsx
@@ -5,12 +5,7 @@ const Youtube: NextPage<YoutubeEmbedData> = ({ videoId }) => {
     // https://developers.google.com/youtube/player_parameters#Parameters
     <iframe
       className="w-full aspect-video"
-      src={`
-        https://www.youtube.com/embed/${videoId}
-          ?rel=0
-          &cc_load_policy=1
-          &cc_lang_pref=en
-      `}
+      src={`https://www.youtube.com/embed/${videoId}?rel=0&cc_load_policy=1&cc_lang_pref=en`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowFullScreen
