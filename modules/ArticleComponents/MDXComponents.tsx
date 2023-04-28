495f81e80b8b1dc44f6f611fe5ecd18ac271485b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-04-28 15:18:16 -0400

    make all images scale on hover

--- modules/ArticleComponents/MDXComponents.tsx
+++ modules/ArticleComponents/MDXComponents.tsx
@@ -15,7 +15,7 @@ const MDXComponents = {
         <Link
           href={src ? "/api/images/" + src : ""}
           target="_blank"
-          className="w-full max-w-full flex justify-center"
+          className="w-full max-w-full flex justify-center overflow-hidden rounded-lg "
         >
           <Image
             src={"/api/images/" + src}
@@ -30,7 +30,8 @@ const MDXComponents = {
             (min-width: 768px) 75vw,
             90vw"
             className={
-              (className ?? "") + " rounded-lg m-0"
+              (className ?? "") +
+              " rounded-lg m-0 object-cover object-center scale-100 hover:scale-[101%] transition-transform duration-500 ease-in-out"
             }
           />
         </Link>
@@ -47,17 +48,15 @@ const MDXComponents = {
       )
     }
 
-    return (
-      imgElem
-      // <span>
-      //   {imgElem}
-      //   {title && (
-      //     <span className="text-center text-gray-500 dark:text-gray-400 text-xs m-0">
-      //       {title}
-      //     </span>
-      //   )}
-      // </span>
-    )
+    return imgElem
+    // <span>
+    //   {imgElem}
+    //   {title && (
+    //     <span className="text-center text-gray-500 dark:text-gray-400 text-xs m-0">
+    //       {title}
+    //     </span>
+    //   )}
+    // </span>
   },
   NationInfoCard,
   BookAndQuill,
