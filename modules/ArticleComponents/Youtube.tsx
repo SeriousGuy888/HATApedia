ee311a6acd8c57c09421475ec734b959f7049222
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-05-09 14:11:48 -0400

    display placeholder for youtube embeds in print mode

--- modules/ArticleComponents/Youtube.tsx
+++ modules/ArticleComponents/Youtube.tsx
@@ -2,14 +2,22 @@ import type { NextPage } from "next"
 
 const Youtube: NextPage<YoutubeEmbedData> = ({ videoId }) => {
   return (
-    // https://developers.google.com/youtube/player_parameters#Parameters
-    <iframe
-      className="w-full aspect-video"
-      src={`https://www.youtube.com/embed/${videoId}?rel=0&cc_load_policy=1&cc_lang_pref=en`}
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowFullScreen
-    ></iframe>
+    <aside className="w-full flex">
+      {/* https://developers.google.com/youtube/player_parameters#Parameters */}
+      <iframe
+        className="w-full aspect-video print:hidden"
+        src={`https://www.youtube.com/embed/${videoId}?rel=0&cc_load_policy=1&cc_lang_pref=en`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+      {/* Display a link to the video in print view */}
+      <div className="hidden print:block w-full border-solid border-2 border-gray-600 rounded-xl">
+        <p className="text-center text-gray-600">
+          <strong>Embedded Video:</strong> youtu.be/{videoId}
+        </p>
+      </div>
+    </aside>
   )
 }
 
