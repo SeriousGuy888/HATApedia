c0e8770e9268f6605bb5b2a615b6d650ce798d5c
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-06-03 01:52:36 -0400

    add exporting weather as image

--- modules/Weather/WeatherCard.tsx
+++ modules/Weather/WeatherCard.tsx
@@ -1,6 +1,19 @@
 import { NextPage } from "next"
 import { TemperatureUnit, WeatherData } from "../../pages/weather"
+import { useRef } from "react"
 import cntl from "cntl"
+import { toPng } from "html-to-image" // https://github.com/tsayen/dom-to-image
+import { saveAs } from "file-saver" // https://github.com/eligrey/FileSaver.js/
+import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded"
+
+// Import Montserrat manually so that the font renders properly when exported as an image
+import { Montserrat } from "@next/font/google"
+const font = Montserrat({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  fallback: ["Helvetica", "Arial", "sans-serif"],
+})
 
 interface Props {
   cardInfo: {
@@ -14,6 +27,39 @@ interface Props {
 const WeatherCard: NextPage<Props> = ({ cardInfo, weatherData, tempUnit }) => {
   const currWeather = weatherData.current
   const unitSymbol = tempUnit === "celsius" ? "°C" : "K"
+
+  const cardElem = useRef<HTMLDivElement | null>(null)
+  const exportButton = useRef<HTMLButtonElement | null>(null)
+  const exportToPng = () => {
+    const elem = cardElem.current
+    if (!elem) {
+      return
+    }
+
+    const elemW = elem.clientWidth
+    const elemH = elem.clientHeight
+    const heightIsLarger = elemH > elemW
+    const largerDimension = 2048
+
+    exportButton.current!!.style.opacity = "0"
+    toPng(elem, {
+      // resize canvas but keep aspect ratio
+      canvasWidth: heightIsLarger
+        ? elemW * (largerDimension / elemH)
+        : largerDimension,
+      canvasHeight: heightIsLarger
+        ? largerDimension
+        : elemH * (largerDimension / elemW),
+    })
+      .then((png: any) => {
+        saveAs(png, "weather.png")
+        exportButton.current!!.style.opacity = "1"
+      })
+      .catch((e) => {
+        console.error(e)
+        alert("Error while exporting image D:")
+      })
+  }
 
   const cardBg = ({ isDay }: { isDay: boolean }) => cntl`
     bg-gradient-to-br
@@ -46,7 +92,8 @@ const WeatherCard: NextPage<Props> = ({ cardInfo, weatherData, tempUnit }) => {
         {
           isDay: !!currWeather.is_day,
         },
-      )}`}
+      )} ${font.className}`}
+      ref={cardElem}
     >
       <header className="flex justify-between gap-4 flex-col-reverse @md:flex-row">
         <section className="flex flex-col items-center @md:items-start">
@@ -83,8 +130,15 @@ const WeatherCard: NextPage<Props> = ({ cardInfo, weatherData, tempUnit }) => {
         <InfoBox title={"Precipitation"} data={`${currWeather.precip_mm}mm`} />
       </ul>
 
-      <footer>
-        <p className="text-right text-[0.5rem] uppercase opacity-30">
+      <footer className="flex justify-between items-center gap-4">
+        <button
+          className="opacity-50 hover:opacity-100"
+          onClick={exportToPng}
+          ref={exportButton}
+        >
+          <DownloadRoundedIcon fontSize="small" sx={{ color: "grey" }} />
+        </button>
+        <p className="flex-grow text-right text-[0.5rem] uppercase opacity-30">
           Data last updated: {currWeather.last_updated}
         </p>
       </footer>
