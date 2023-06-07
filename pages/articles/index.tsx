7318f70c4075b6730131f6335c19aa40c4abb2cf
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-06-07 16:02:23 -0400

    fix article directory's inconsistent width

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -7,7 +7,7 @@ const ArticlesDirectory = () => {
       <Head>
         <title>Articles - HATApedia</title>
       </Head>
-      <article className="h-fit p-8 max-w-full md:max-w-5xl">
+      <article className="h-fit p-8 w-full md:max-w-5xl">
         <h1 className="text-4xl mb-8">Articles</h1>
 
         <ArticleSearch />
