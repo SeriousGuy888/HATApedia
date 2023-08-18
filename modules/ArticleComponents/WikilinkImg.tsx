765dea949e1ea54dc500fa7349c4a73398d2673e
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2023-08-18 18:35:07 -0400

    fix wikilink image hover effect

--- modules/ArticleComponents/WikilinkImg.tsx
+++ modules/ArticleComponents/WikilinkImg.tsx
@@ -9,26 +9,28 @@ const WikilinkImg: NextPage<HTMLImageElement> = (imageData) => {
   const context = useContext(ImageViewerContext)
 
   return (
-    <Image
-      src={"/images/content/" + src}
-      blurDataURL="/images/loading.png"
-      placeholder="blur"
-      quality={20}
-      title={title}
-      alt={alt ?? ""}
-      width={width}
-      height={height}
-      sizes="(min-width: 1280px) 1024px,
-            (min-width: 768px) 75vw,
-            90vw"
-      className={
-        (className ?? "") +
-        " rounded-lg m-0 object-cover object-center scale-100 hover:scale-[101%] transition-transform duration-500 ease-in-out cursor-pointer"
-      }
-      onClick={() => {
-        context.setImage(imageData)
-      }}
-    />
+    <span className="flex overflow-clip rounded-lg">
+      <Image
+        src={"/images/content/" + src}
+        blurDataURL="/images/loading.png"
+        placeholder="blur"
+        quality={20}
+        title={title}
+        alt={alt ?? ""}
+        width={width}
+        height={height}
+        sizes="(min-width: 1280px) 1024px,
+      (min-width: 768px) 75vw,
+      90vw"
+        className={
+          (className ?? "") +
+          " m-0 object-cover scale-100 hover:scale-[102%] transition-transform duration-500 ease-in-out cursor-pointer"
+        }
+        onClick={() => {
+          context.setImage(imageData)
+        }}
+      />
+    </span>
   )
 }
 
